ed95dcc501d875dd60c75be6d0368fc462637f81
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-05-12 00:14:03 +0430

    test Rockets component

--- src/components/Profile/MissionFilter.js
+++ src/components/Profile/MissionFilter.js
@@ -1,26 +1,26 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-// export default function MissionFilter({ missions }) {
-//   return (
-//     <div className="reserved-container">
-//       <h2>My Missions</h2>
-//       {missions.length === 0 && <h3>There are no missions reserved</h3>}
-//       {missions.length > 0 && (
-//         <ul className="rockets-reserved">
-//           {missions.map((mission) => (
-//             <li key={mission.id}>{mission.name}</li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// }
+export default function MissionFilter({ missions }) {
+  return (
+    <div className="reserved-container">
+      <h2>My Missions</h2>
+      {missions.length === 0 && <h3>There are no missions reserved</h3>}
+      {missions.length > 0 && (
+        <ul className="rockets-reserved">
+          {missions.map((mission) => (
+            <li key={mission.id}>{mission.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
 
-// MissionFilter.propTypes = {
-//   missions: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string,
-//       name: PropTypes.string,
-//     }),
-//   ).isRequired,
-// };
+MissionFilter.propTypes = {
+  missions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+    }),
+  ).isRequired,
+};
